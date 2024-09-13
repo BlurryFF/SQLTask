15361a241e64ad594efca8695f58d0b661543b4c
Georgiy <115817252+BlurryFF@users.noreply.github.com>
2024-09-13 22:09:13 +0300

    Create Products.sql

---
+++ Products.sql
@@ -0,0 +1,5 @@
+CREATE Table Products (
+  Id INT PRIMARY KEY,
+  name varchar(255) NOT NULL);
+
+INSERT INTO Products VALUES(1, 'Телевизор'), (2, 'Телефон'), (3, 'Микроволновка'), (4, 'Футболка'), (5, 'Шорты'), (6, 'Окно');
